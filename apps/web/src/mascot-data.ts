263c6bf0de64a3df88dfea6dff00bc87508c7803
stevegirouxpaquette-art <stevegirouxpaquette@gmail.com>
2026-08-26 08:05:56 -0400

    Use exact original FaisLaJob mascot PNG on homepage

--- apps/web/src/mascot-data.ts
+++ apps/web/src/mascot-data.ts
@@ -1,6 +1,1 @@
-import p0 from './mascot-home-part-0';
-import p1 from './mascot-home-part-1';
-import p2 from './mascot-home-part-2';
-import p3 from './mascot-home-part-3';
-
-export default 'data:image/webp;base64,' + p0 + p1 + p2 + p3;
+export default '/faislajob-mascot-exact.png';
