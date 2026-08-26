e13809365964817d1b3f5b760256629f83b9ad6a
stevegirouxpaquette-art <stevegirouxpaquette@gmail.com>
2026-08-26 08:50:52 -0400

    Use approved transparent FaisLaJob mascot on homepage

--- apps/web/src/mascot-data.ts
+++ apps/web/src/mascot-data.ts
@@ -1,6 +1,2 @@
-import p0 from './mascot-home-part-0';
-import p1 from './mascot-home-part-1';
-import p2 from './mascot-home-part-2';
-import p3 from './mascot-home-part-3';
-
-export default 'data:image/webp;base64,' + p0 + p1 + p2 + p3;
+import mascotExact from './assets/mascotte-faislajob-transparent.webp';
+export default mascotExact;
