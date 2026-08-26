0a315ab40b0215a8f6e610246e565094a587d4ca
stevegirouxpaquette-art <stevegirouxpaquette@gmail.com>
2026-08-26 07:40:10 -0400

    Replace homepage mascot with new FaisLaJob character

--- apps/web/src/mascot-data.ts
+++ apps/web/src/mascot-data.ts
@@ -1,8 +1,1 @@
-import p0 from './mascot-part-0';
-import p1 from './mascot-part-1';
-import p2 from './mascot-part-2';
-import p3 from './mascot-part-3';
-import p4 from './mascot-part-4';
-import p5 from './mascot-part-5';
-
-export default 'data:image/webp;base64,' + p0 + p1 + p2 + p3 + p4 + p5;
+export default '/faislajob-mascot-new.webp';
