9ad53fd966b16be9360b008520dcd21431444b82
stevegirouxpaquette-art <stevegirouxpaquette@gmail.com>
2026-08-26 07:56:25 -0400

    Use optimized new FaisLaJob mascot on homepage

--- apps/web/src/mascot-data.ts
+++ apps/web/src/mascot-data.ts
@@ -1,1 +1,6 @@
-export default '/faislajob-mascot-new.webp';
+import p0 from './mascot-home-part-0';
+import p1 from './mascot-home-part-1';
+import p2 from './mascot-home-part-2';
+import p3 from './mascot-home-part-3';
+
+export default 'data:image/webp;base64,' + p0 + p1 + p2 + p3;
