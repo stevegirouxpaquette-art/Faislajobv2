e739b179c906dc490660da85cda350aa34c4b7b8
stevegirouxpaquette-art <stevegirouxpaquette@gmail.com>
2026-08-26 08:32:17 -0400

    Embed FaisLaJob mascot directly to prevent broken image

--- apps/web/src/mascot-data.ts
+++ apps/web/src/mascot-data.ts
@@ -1,2 +1,6 @@
-import mascotExact from './assets/faislajob-mascot-exact.png';
-export default mascotExact;
+import p0 from './mascot-home-part-0';
+import p1 from './mascot-home-part-1';
+import p2 from './mascot-home-part-2';
+import p3 from './mascot-home-part-3';
+
+export default 'data:image/webp;base64,' + p0 + p1 + p2 + p3;
